fb611ea1e812d850a287d57a1dd611ba118b2419
John Benjamin McCarthy <tazerenix@gmail.com>
2024-11-13 19:21:33 +1100

    Made API caller out of order until I fix API bugs with double advisors etc.

--- src/GraphSelector.tsx
+++ src/GraphSelector.tsx
@@ -14,18 +14,23 @@ export function GraphSelector(props: { presetFunction: (preset: Preset) => void 
     return (
         <>
             <div id={"selector"}>
-                <span id={"genealogyIdText"}>Maths Genealogy URL or ID number:</span>
-                <input type={"text"} id={"genealogyId"} placeholder={"https://www.mathgenealogy.org/id.php?id=74313"}/>
-                <input type={"number"} id={"maxGenerationsUp"} placeholder={"5"} min={0}/>
-                <input type={"number"} id={"maxGenerationsDown"} placeholder={"5"} min={0}/>
-                <button id={"genealogyIdButton"}>Go!</button>
+                <span className={"APIinput"}>
+                    <span className={"disabled"}>OUT OF ORDER!!</span>
+                    <span id={"genealogyIdText"}>Maths Genealogy URL or ID number:</span>
+                    <input type={"text"} id={"genealogyId"}
+                           placeholder={"https://www.mathgenealogy.org/id.php?id=74313"}/>
+                    <input type={"number"} id={"maxGenerationsUp"} placeholder={"5"} min={0}/>
+                    <input type={"number"} id={"maxGenerationsDown"} placeholder={"5"} min={0}/>
+                    <button id={"genealogyIdButton"}>Go!</button>
+                </span>
                 <span id={"spacer"}></span>
                 <span id={"presetText"}>Choose a preset:</span>
                 <select name={"presets"} id={"presets"} defaultValue={"johnbenjaminmccarthy"} onChange={handlePresetChange}>
-                    <option key={Preset.johnbenjaminmccarthy} value={"johnbenjaminmccarthy"}>John</option>
-                    <option key={Preset.johnbenjaminmccarthybig} value={"johnbenjaminmccarthybig"}>John Big</option>
-                    <option key={Preset.ruadhaidervan} value={"ruadhaidervan"}>Ruadhai Dervan</option>
-                    <option key={Preset.kellifrancisstaite} value={"kellifrancisstaite"}>Kelli Francis-Staite</option>
+                    <option key={Preset.johnbenjaminmccarthy} value={"johnbenjaminmccarthy"}>John McCarthy</option>
+                    <option key={Preset.johnbenjaminmccarthybig} value={"johnbenjaminmccarthybig"}>John McCarthy (Big!)</option>
+                    {//<option key={Preset.ruadhaidervan} value={"ruadhaidervan"}>Ruadhai Dervan</option>
+                    //<option key={Preset.kellifrancisstaite} value={"kellifrancisstaite"}>Kelli Francis-Staite</option>
+                    }
                     <option key={Preset.simondonaldson} value={"simondonaldson"}>Simon Donaldson</option>
                     <option key={Preset.michaelatiyah} value={"michaelatiyah"}>Michael F. Atiyah</option>
                     <option key={Preset.danielquillen} value={"danielquillen"}>Daniel Quillen</option>
